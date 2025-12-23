4933d4754e3a30ce80ee9f0c64defef9f0b88f96
Anish-100 <anish10707@gmail.com>
2025-12-22 21:44:23 -0800

    Graphing stock data over time
    Uses API to get monthly data from Alpha-Vantage, saves it based on the ticker's name.
    
    Graphs this data using matplotlib, have to fix the x-axis.

--- src/api.py
+++ src/api.py
@@ -1,6 +1,8 @@
 import requests
 import os
+import json
 from dotenv import load_dotenv
+from pathlib import Path
 
 load_dotenv()
 api_key = os.getenv('API_KEY')
@@ -16,5 +18,59 @@ class COMMODITY:
         user_data = {'function':'wheat','interval':'annual','datatype':'json','apikey' : api_key}
         temp_wheat = requests.get('https://www.alphavantage.co/query?function=WHEAT',params=user_data)
         return temp_wheat.json()
+
+class STOCK:
+    def __init__(self):
+        self.stock_info = ''
+        self.symbol = ''
+        self.base_path = Path('./src/data')
+    
+    def get_data(self,symbol:str):
+        self.symbol = symbol
+        stock_data = None
+
+        if self.check_if_exists() == False:
+            stock_data = self.get_api_data()
+            with open(f'{self.base_path}/{symbol}.txt','w') as json_file:
+                json.dump(stock_data, json_file, indent=4)
+        else:
+            stock_data = self.get_file_data()
+
+        return stock_data
+
+        
+
+    
+    def get_file_data(self):
+        '''Gets the data from a local file instead'''
+        with open (f'{self.base_path}/{self.symbol}.txt', 'r') as file:
+            return json.load(file)
+                    
+    def get_api_data(self):
+        print("API USED!!!")
+        symbol = self.symbol
+        user_data = {'function':'TIME_SERIES_MONTHLY_ADJUSTED','symbol':symbol,'datatype':'json','apikey' : api_key}
+        stock_data = requests.get('https://www.alphavantage.co/query',params=user_data)
+        print(stock_data.json())
+        return stock_data.json()
+    
+
+    def check_if_exists(self):
+        for file in self.base_path.iterdir():
+            print('FILE',file)
+            file = Path(file)
+            if not file.is_file():
+                pass
+            else:
+                if file.stem == self.symbol:
+                    return True
+                else:
+                    return False
+        return False
+
+
+        
+
+
 
         
--- src/ui.py
+++ src/ui.py
@@ -3,11 +3,11 @@ import matplotlib.pyplot as plt
 
 
 
-def print_world():
+def print_wheat():
+    '''Just a test function that gets the value of wheat and plots it over time'''
     commodity = api.COMMODITY()
     plot_data = commodity.get_wheat()
     data = plot_data["data"]
-   # print('data',data)
     value_dict = {}
     print('data',data)
     for element in reversed(data):
@@ -28,7 +28,52 @@ def print_world():
     plt.xticks(rotation=45, ha='right')
     plt.show()
 
+
+def plot_data(time_data,symbol)->None:
+    x_list = []
+    for element in time_data:
+        x_list.append(element[:-3])
+    plt.plot(x_list,list(time_data.values()))
+    plt.xlabel('Dates')
+    plt.ylabel('Value Over Time')
+    plt.ylim(0, None)
+    plt.title(f'Price of {symbol} over time' )
+    plt.grid(True,alpha=0.5)
+    plt.xticks(rotation=45, ha='right')
+    plt.show()
+
+
+def stock_value(symbol):
+    stock = api.STOCK()
+    stock_data = stock.get_data(symbol)
+    print('STOCK')
+    get_meta_data(stock_data)
+    time_data = print_stock_data(stock_data)
+    plot_data(time_data,symbol)
+
+def get_meta_data(stock_data : 'json') -> None:
+    '''Prints all the initial info based on the stock data'''
+    meta_data = stock_data['Meta Data']
+    print('Ticker is',meta_data['2. Symbol'])
+    print('Last Refreshed',meta_data['3. Last Refreshed'])
+    print('Time Zone',meta_data['4. Time Zone'])
+
+def print_stock_data(stock_data:'json')-> 'json':
+    time_series_data = stock_data['Monthly Adjusted Time Series']
+    date_dict = {}
+    for date in reversed(time_series_data):
+        data = time_series_data[date]
+        avg = float(data['5. adjusted close'])
+        date_dict[date] = avg
+    return date_dict
+
+
+
+def run_loop():
+    symbol = input('Please enter a symbol which you want data of: ')
+    stock_value(symbol)
+
 if __name__ == '__main__':
-    print_world()
+    run_loop()
 
 
